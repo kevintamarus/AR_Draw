8610ddd131c7dd733898c6798337c0e1755fb02d
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-08-26 15:58:03 -0700

    navigation done

--- client/index.android.js
+++ client/index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,16 +5,87 @@ import {
   Text,
   View
 } from 'react-native';
-import App from './components/App'
+import { Router, Scene } from 'react-native-router-flux';
 
-export default class AR extends Component {
-  render() {
-    return (
-      <View>
-        <App navigation={this.navigation}/>
-      </View>
-    );
-  }
+import Draw from './components/Draw';
+import Friends from './components/Friends';
+import HomePage from './components/HomePage';
+import Nearby from './components/Nearby';
+import Notifications from './components/Notifications';
+import OtherUsers from './components/OtherUsers';
+import Photos from './components/Photos';
+import Profile from './components/Profile';
+import Settings from './components/Settings';
+import ViewFinder from './components/ViewFinder';
+
+const AR = () => {
+  return (
+    <Router>
+      <Scene key="root">
+        <Scene key="homepage"
+          component={HomePage}
+          title="Home Page"
+          initial
+        />
+        <Scene key="settings"
+          component={Settings}
+          title="Settings"
+        />
+
+        <Scene key="viewfindermain" tabs={true}>
+          <Scene
+            key="viewfinder"
+            component={ViewFinder}
+            title="View Finder"
+          />
+          <Scene
+            key="nearby"
+            component={Nearby}
+            title="Nearby"
+          />
+        </Scene>
+
+        <Scene key="photos"
+          component={Photos}
+          title="Photos"
+        />
+        <Scene key="profilemain" tabs={true}>
+          <Scene
+            key="profile"
+            component={Profile}
+            title="Profile"
+          />
+          <Scene
+            key="friends"
+            component={Friends}
+            title="Friends"
+          />
+          <Scene
+            key="other users"
+            component={OtherUsers}
+            title="Other Users"
+          />
+        </Scene>
+
+        <Scene
+          key="draw"
+          component={Draw}
+          title="Draw"
+        />
+        <Scene
+          key="notifications"
+          component={Notifications}
+          title="Notifications"
+        />
+      </Scene>
+    </Router>
+  );
 }
+
+const styles = StyleSheet.create({
+  tabbar: {
+    alignItems: 'flex-start'
+  },
+});
 
 AppRegistry.registerComponent('AR', () => AR);
